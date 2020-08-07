e3c4cd84e7d60d0c3b932fe987d0cd41d996270a
Luca Capra <luca.capra@gmail.com>
2020-08-07 13:41:04 +0200

    updated docker compose

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Ambianic Quick Start
 
 A collection of files and scripts to run ambianic.ai on your Raspberry Pi
+
+## Quick install on Raspberry Pi or Debian-based distro
+
+```sh
+wget -qO - https://raw.githubusercontent.com/ambianic/ambianic-quickstart/master/setup.sh | sh
+```
+
+## Troubleshooting the Picamera
+
+- Read the offcial Picamera [troubleshooting section](https://www.raspberrypi.org/documentation/raspbian/applications/camera.md)
+- Ensure to run `raspi-config` and enable the video source under the Interfacing menu
+- See if the camera is available with `vcgencmd get_camera` and expect to have `supported=1 detected=1`
+- Use `raspistill -o image.jpg` to test the camera connection and ensure no errors are shown
+
+If  you get an error ensure the camera is connected to the pins in the center of the board (labelled as camera)
--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+
+BASE_URL=https://raw.githubusercontent.com/ambianic/ambianic-quickstart/master
+AMBIANIC_COMPOSE_YAML=$BASE_URL/docker-compose.yaml
+AMBIANIC_CFG=$BASE_URL/config.yaml
+
 sudo true
 echo "Installing Ambianic.ai"
 
@@ -15,6 +20,9 @@ if ! type "docker-compose" > /dev/null; then
     # on PI
     sudo apt-get install -y libffi-dev libssl-dev  python3 python3-pip
     sudo pip3 install docker-compose
+    # enable pi camera as /dev/video*
+    echo "bcm2835-v4l2" | sudo tee -a /etc/modules
+    sudo modprobe bcm2835-v4l2    
   else
     #other linux
     sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -29,12 +37,12 @@ sudo mkdir -p ${DEST_DIR}
 sudo chown ${USER} ${DEST_DIR}
 
 # get docker-compose.yaml
-wget https://gist.githubusercontent.com/ivelin/3891a7b5d61a12d6a1b9f652b6d53dce/raw/452658c6372f3ebf631e8e1c8567507d74e6e922/docker-compose.yaml
+wget $AMBIANIC_COMPOSE_YAML
 
 # get config.yml
-sudo wget https://gist.githubusercontent.com/ivelin/1d1c885a25ad45bf8a3262653944b82c/raw/704aafa94b10fd2bdb506e8f98e12fa74cfac7e4/config.yaml -O ${DEST_DIR}/config.yaml
+sudo wget $AMBIANIC_CFG -O ${DEST_DIR}/config.yaml
 
 #sudo su - ${USER}
 sudo docker-compose up -d
-echo "Ambianic started, run `sudo docker-compose logs` to see the status"
+echo "\n\nAmbianic started, run `sudo docker-compose logs` to see the status"
 exit 0
