14ba5e5bde58bf9a40321359a00b43c32adfa970
Luca Capra <luca.capra@gmail.com>
2020-10-10 10:17:47 +0200

    force removal of previous containers

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,7 +18,7 @@ then
 fi
 
 # Attempt to kill overlapping containers
-docker stop ambianic-edge ambianic-watchtower && docker rm -f ambianic-edge ambianic-watchtower || true
+docker stop ambianic-edge ambianic-watchtower || true && docker rm -f ambianic-edge ambianic-watchtower || true
 sudo docker-compose up -d --remove-orphans
 sleep 2
 echo "Ambianic.ai Edge is starting, enjoy! "
